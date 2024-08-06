e6283b021b119dcf81756568e13f36d6588143ab
“Brian <brian.anggriawan@komunal.co.id>
2024-08-06 19:43:28 +0700

    Initial Commit

---
+++ ListNews-Swiftui/ListNews_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNews_SwiftuiApp.swift
+//  ListNews-Swiftui
+//
+//  Created by Brian Anggriawan on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ListNews_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
